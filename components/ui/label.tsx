596333eea9b047b8672ff59895b38300134b530e
Wolfsquad9 <guy.vodor@me.com>
2025-11-22 09:50:04 +0100

    Update label.tsx

--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -4,7 +4,8 @@ import * as React from "react"
 import * as LabelPrimitive from "@radix-ui/react-label"
 import { cn } from "@/lib/utils"
 
-const Label = React.forwardRef
+// 🛠️ CORRECTED SYNTAX
+const Label = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>
 >(({ className, ...props }, ref) => (
